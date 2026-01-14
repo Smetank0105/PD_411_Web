d30a4a4fa7beadc5a2311ae859bd29a796c427ae
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-01-14 19:40:12 +0700

    Factorial DONE

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+﻿// JavaScript source code
+function Factorial(n) {
+	let f = BigInt(1);
+	//document.write("<pre>");
+	for (let i = 1n; i <= n; i++) {
+		f *= i;
+		//document.writeln(`${i}! = ${f};`);
+	}
+	//document.write("</pre>");
+	return f;
+}
+function Factorial2() {
+	let source_input_field = document.getElementById("factorial-source");
+	let source_value = source_input_field.value;
+	let factorial_result = document.getElementById("factorial-result");
+	factorial_result.innerHTML = Factorial(source_value);
+}
