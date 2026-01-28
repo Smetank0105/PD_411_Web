5bde57fa13e9b94a293b48224a6c2ceea9f14064
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-01-28 19:27:27 +0700

    DOM animation START

---
+++ JavaScript/DOM/dom.js
@@ -0,0 +1,21 @@
+﻿// JavaScript source code
+document.title = 'DOM';
+//document.write('<h1>Document Object Model</h1>');
+//document.write(document.title);
+let header1 = document.createElement('h1');
+header1.innerHTML = 'Document Object Model';
+document.body.prepend(header1);
+
+document.body.bgColor = '#00C8C8C8';
+
+let children = document.body.children;
+console.log(children);
+children[0].innerHTML = 'Объектная модель документа';
+console.log(children[0].outerHTML);
+//children[0].outerHTML = '<h2>DOM</h2>';
+console.log(document.body.outerHTML);
+let paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs.length);
+let statistics = document.createElement('div');
+statistics.innerHTML = `Кол-во абзацев: ${paragraphs.length}; Кол-во заголовков: ${document.getElementsByTagName('h2').length}; Кол-во картинок: ${document.getElementsByTagName("img").length}`;
+document.body.prepend(statistics);
